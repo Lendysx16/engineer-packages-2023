7011d33a21a61bf7c3a42cf92058d98a30e91aea
Lendysx16 <91876886+Lendysx16@users.noreply.github.com>
2023-06-08 11:39:38 +0300

    Update evolventa.m
    tmp version

--- octave_tasks/evolventa.m
+++ octave_tasks/evolventa.m
@@ -1,20 +1,30 @@
-function evolventa(x = @(t) sin(t), y = @(t) (cos(t)), start_t = 0, end_t = 4 * pi)
+function evolventa(x = @(t) sin(t), y = @(t) (cos(t)), start_t = 0, end_t = 4 * pi, step = 0.01)
     pkg load symbolic;
     x = sym(x);
     y = sym(y);
     syms t;
     dx_dt = diff(x);
     dy_dt = diff(y);
-
+    x = function_handle(x);
+    y = function_handle(y);
     tmp = sqrt(dx_dt.^2 + dy_dt.^2);
-
-    X = x - (dx_dt * int(tmp, 0, t)) / (tmp);
-    Y = y - (dy_dt * int(tmp, 0, t)) / (tmp);
-    X = function_handle(X);
-    Y = function_handle(Y);
+    dx_dt = function_handle(x);
+    dy_dt = function_handle(y);
+    tmp = function_handle(tmp);
+    % X = x - (dx_dt * int(tmp, 0, t)) / (tmp);
+    % Y = y - (dy_dt * int(tmp, 0, t)) / (tmp);
+    myintegral = @(f) @(t) quad(f, 0, t);
+    
+    
+    t = start_t:step:end_t;
+    X = zeros(1, length(t));
+    Y = zeros(1, length(t));
+    for k = 1:length(t):
+        X(k) = x(t) - ( dx_dt(t) *  myintegral(tmp, k) ) / tmp(t);
+        Y(k) = y(t) - ( dy_dt(t) *  myintegral(tmp, k) ) / tmp(t);
+        
+    
     figure;
-    t = start_t:0.01:end_t;
-
     hold on;
     min_x = min(X(t));
     max_x = max(X(t));
@@ -27,7 +37,6 @@ function evolventa(x = @(t) sin(t), y = @(t) (cos(t)), start_t = 0, end_t = 4 * 
 
     for k = 1:5:length(t) - 5
         plot(X(t(k:k + 5)), Y(t(k:k + 5)), 'b');
-        hold on;
-        pause(0.01);
+        drawnow;
 
     end

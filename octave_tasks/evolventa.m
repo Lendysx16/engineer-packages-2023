d1e2a717c04b627e404c29705a024014cf4030b4
Lendysx16 <91876886+Lendysx16@users.noreply.github.com>
2023-06-08 12:31:44 +0300

    Update evolventa.m

--- octave_tasks/evolventa.m
+++ octave_tasks/evolventa.m
@@ -1,4 +1,4 @@
-function evolventa(x = @(t) sin(t), y = @(t) (cos(t)), start_t = 0, end_t = 4 * pi, step = 0.01)
+function evolventa(x = @(t) 2 * sin(t).^2, y = @(t) 4 * (cos(t)), start_t = 0, end_t = 4 * pi, step = 0.01)
     pkg load symbolic;
     x = sym(x);
     y = sym(y);
@@ -8,35 +8,33 @@ function evolventa(x = @(t) sin(t), y = @(t) (cos(t)), start_t = 0, end_t = 4 * 
     x = function_handle(x);
     y = function_handle(y);
     tmp = sqrt(dx_dt.^2 + dy_dt.^2);
-    dx_dt = function_handle(x);
-    dy_dt = function_handle(y);
+    dx_dt = function_handle(dx_dt);
+    dy_dt = function_handle(dy_dt);
     tmp = function_handle(tmp);
     % X = x - (dx_dt * int(tmp, 0, t)) / (tmp);
     % Y = y - (dy_dt * int(tmp, 0, t)) / (tmp);
     myintegral = @(f) @(t) quad(f, 0, t);
-    
-    
+    integ_tmp = myintegral(tmp);
+
     t = start_t:step:end_t;
     X = zeros(1, length(t));
     Y = zeros(1, length(t));
-    for k = 1:length(t):
-        X(k) = x(t) - ( dx_dt(t) *  myintegral(tmp, k) ) / tmp(t);
-        Y(k) = y(t) - ( dy_dt(t) *  myintegral(tmp, k) ) / tmp(t);
-        
-    
-    figure;
-    hold on;
-    min_x = min(X(t));
-    max_x = max(X(t));
-    min_y = min(Y(t));
-    max_y = max(Y(t));
-    axis equal;
-    grid on;
-    xlim([min_x - 5, max_x + 5]);
-    ylim([min_y - 5, max_y + 5]);
+    for k = 1:length(t)
+        X(k) = x(t(k)) - ( dx_dt(t(k)) *  integ_tmp(t(k)) ) / tmp(t(k));
+        Y(k) = y(t(k)) - ( dy_dt(t(k)) *  integ_tmp(t(k)) ) / tmp(t(k));
+    end;
 
-    for k = 1:5:length(t) - 5
-        plot(X(t(k:k + 5)), Y(t(k:k + 5)), 'b');
+    ;
+    figure;
+    h = plot(X(1 : 5), Y(1: 5), 'b');
+    grid on;
+    xlim([min(X) - 5, max(X) + 5]);
+    ylim([min(Y) - 5, max(Y) + 5]);
+    hold on;
+    axis equal
+    z = length(X) / 10;
+    for k = 1:length(X) - z
+        set(h, 'XData', X(1:k+z), "YData", Y(1:k+z));
         drawnow;
 
     end
